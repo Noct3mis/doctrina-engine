4d9568a100790bb1b7268b31f805ed05396507ad
Noct3mis <nathan.handfield1@gmail.com>
2024-09-09 08:48:45 -0400

    refactor: extract method buildRenderingHints

--- src/Game.java
+++ src/Game.java
@@ -28,10 +28,8 @@ public class Game {
 
         while (playing) {
             bufferedImage = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
-            RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
             bufferEngine = bufferedImage.createGraphics();
-            bufferEngine.setRenderingHints(hints);
+            bufferEngine.setRenderingHints(buildRenderingHints());
 
             update();
             drawOnBuffer();
@@ -48,6 +46,12 @@ public class Game {
             }
         }
         updateSyncTime();
+    }
+
+    private RenderingHints buildRenderingHints() {
+        RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        return hints;
     }
 
     private void update() {
