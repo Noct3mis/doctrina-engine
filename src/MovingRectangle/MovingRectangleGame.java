592b91d16b17cc6d6bce40fd26a28e78d0bb6b8a
Noct3mis <nathan.handfield1@gmail.com>
2024-09-18 13:48:30 -0400

    feat: amazing new game

---
+++ src/MovingRectangle/MovingRectangleGame.java
@@ -0,0 +1,21 @@
+package MovingRectangle;
+
+import Doctrina.Canvas;
+import Doctrina.Game;
+
+public class MovingRectangleGame extends Game {
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void draw(Canvas canvas) {
+
+    }
+}
