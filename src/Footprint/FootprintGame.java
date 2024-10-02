10d61fb188c95d03884df86b47abefa0820cfbff
Noct3mis <nathan.handfield1@gmail.com>
2024-10-02 14:16:04 -0400

    feat: implement two player

--- src/Footprint/FootprintGame.java
+++ src/Footprint/FootprintGame.java
@@ -8,25 +8,39 @@ import java.util.ArrayList;
 
 public class FootprintGame extends Game {
 
-    private GamePad gamePad;
-    private Player player;
+    private GamePad gamePadOne;
+    private GamePad gamePadTwo;
+    private Player playerOne;
+    private Player playerTwo;
     private ArrayList<Footprint> footprints;
 
     @Override
     protected void initialize() {
-        gamePad = new GamePad();
-        player = new Player(gamePad);
+        gamePadOne = new GamePad();
+
+        gamePadTwo = new GamePad();
+        gamePadTwo.useWASDKeys();
+
+        playerOne = new Player(gamePadOne);
+        playerTwo = new Player(gamePadTwo);
+
         footprints = new ArrayList<>();
     }
 
     @Override
     protected void update() {
-        if (gamePad.isQuitPressed()) {
+        if (gamePadOne.isQuitPressed()) {
             stop();
         }
-        player.update();
-        if (gamePad.isMoving()) {
-            footprints.add(player.layFootprint());
+
+        playerOne.update();
+        if (gamePadOne.isMoving()) {
+            footprints.add(playerOne.layFootprint());
+        }
+
+        playerTwo.update();
+        if (gamePadTwo.isMoving()) {
+            footprints.add(playerTwo.layFootprint());
         }
     }
 
@@ -36,6 +50,7 @@ public class FootprintGame extends Game {
         for (Footprint footprint : footprints) {
             footprint.draw(canvas);
         }
-        player.draw(canvas);
+        playerOne.draw(canvas);
+        playerTwo.draw(canvas);
     }
 }
