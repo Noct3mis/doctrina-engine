f34f6d7631e38a9a7f982b3e7da0bec570470278
Noct3mis <nathan.handfield1@gmail.com>
2024-10-02 15:13:51 -0400

    feat: fire missile

--- src/Tank/Tank.java
+++ src/Tank/Tank.java
@@ -8,6 +8,8 @@ import java.awt.*;
 
 public class Tank extends ControllableEntity {
 
+    private int cooldown = 0;
+
     public Tank(MovementController controller) {
         super(controller);
         setDimension(30, 30);
@@ -15,9 +17,21 @@ public class Tank extends ControllableEntity {
         teleport(300, 300);
     }
 
+    public Missile fire() {
+        cooldown = 40;
+        return new Missile(this);
+    }
+
+    public boolean canFire() {
+        return cooldown == 0;
+    }
+
     @Override
     public void update() {
         moveWithController();
+        if (!canFire()) {
+            cooldown--;
+        }
     }
 
     @Override
