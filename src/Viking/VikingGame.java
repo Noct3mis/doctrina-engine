3c5f2fe7799e54d5184c9d0e60830ffea0c6e95e
Noct3mis <nathan.handfield1@gmail.com>
2024-10-23 15:17:10 -0400

    feat: implement fire audio and music

--- src/Viking/VikingGame.java
+++ src/Viking/VikingGame.java
@@ -3,10 +3,17 @@ package Viking;
 import Doctrina.Canvas;
 import Doctrina.Game;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class VikingGame extends Game {
     private Player player;
     private GamePad gamePad;
     private World world;
+    private Tree tree;
+
+    private int soundCooldown;
 
     @Override
     protected void initialize() {
@@ -14,6 +21,18 @@ public class VikingGame extends Game {
         player = new Player(gamePad);
         world = new World();
         world.load();
+        tree = new Tree();
+        tree.load();
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream("audios/music.wav"));
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -22,11 +41,36 @@ public class VikingGame extends Game {
             stop();
         }
         player.update();
+
+        soundCooldown--;
+        if (soundCooldown < 0) {
+            soundCooldown = 0;
+        }
+
+        if (gamePad.isFirePressed() && soundCooldown == 0) {
+            soundCooldown = 10;
+
+            try {
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getClassLoader().getResourceAsStream("audios/fire.wav"));
+                clip.open(audioInputStream);
+                clip.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas);
-        player.draw(canvas);
+
+        if (player.getY() < tree.getY() + 52) {
+            player.draw(canvas);
+            tree.draw(canvas);
+        } else {
+            tree.draw(canvas);
+            player.draw(canvas);
+        }
     }
 }
